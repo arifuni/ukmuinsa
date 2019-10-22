7f36bf1813ea56b79755dc2467d54a34f708d3c9
mubar3 <55468410+mubar3@users.noreply.github.com>
2019-10-22 15:08:41 +0700

    Update chat_model.php

--- application/models/chat_model.php
+++ application/models/chat_model.php
@@ -15,26 +15,5 @@ class chat_model extends CI_Model
     }
 
 
-    public function login(){
-        $query = $this->db->get_where('mhs',
-            array(
-            'niknim' => $this->input->post('niknim'),
-        ));
 
-        $row = $query->result();
-        $num_rows = $query->num_rows();
-
-        if( $num_rows == 1 && password_verify($this->input->post('password'), $row[0]->pass) ){
-
-
-            $newdata = array(
-              'niknim'     => $row[0]->niknim,
-            );
-
-            $this->session->set_userdata($newdata);
-
-
-            return $num_rows;
-        }
     }
-}
